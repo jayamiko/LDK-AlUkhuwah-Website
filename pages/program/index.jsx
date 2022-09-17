dcc6f705fbb8ef12eaf37ec5647a5f9b5a48fb4d
jayamiko <jayamiko4@gmail.com>
2022-09-17 20:09:01 +0700

    Preparing Pages before Coming Soon page

--- pages/program/index.jsx
+++ pages/program/index.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { blog } from "../../data/data";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import styles from "./program.module.css"
+import styles from "./program.module.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -10,29 +10,27 @@ import "swiper/css/navigation";
 
 // import required modules
 import { Autoplay, Pagination } from "swiper";
-import Title from '../../components/utils/Title';
+import Title from "../../components/utils/Title";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import CourseCard from '../../components/Card/CourseCard';
-import TabFilter from '../../components/utils/TabFilter';
-import SearchNotFound from '../../components/Animations/Search/Search';
-
+import CourseCard from "../../components/Card/CourseCard";
+import TabFilter from "../../components/utils/TabFilter";
+import SearchNotFound from "../../components/Animations/Search/Search";
 
 function ProgramPage() {
-
   const carouselArray = [
     styles.carouselCover1,
     styles.carouselCover2,
     styles.carouselCover3,
-    styles.carouselCover4
+    styles.carouselCover4,
   ];
 
   const [query, setQuery] = useState("");
   const [resultQuery, setResultQuery] = useState([]);
-  const [resultData, setResultData] = useState([])
+  const [resultData, setResultData] = useState([]);
 
   const searching = () => {
-    const resultSearch = blog.filter(item => {
+    const resultSearch = blog.filter((item) => {
       const title = item.title.toLowerCase().includes(query.toLowerCase());
       if (query === "") {
         return item;
@@ -41,36 +39,39 @@ function ProgramPage() {
       }
     });
     setResultQuery(resultSearch);
-    setResultData(resultSearch)
-  }
+    setResultData(resultSearch);
+  };
 
   useEffect(() => {
-    searching()
-  }, [query, Autoplay])
+    searching();
+  }, [query, Autoplay]);
 
   return (
     <>
-      <div className='relative h-screen'>
-        <div className='absolute h-screen w-full z-40 flex flex-col justify-center items-center'>
-          <div className='w-full h-3/5 flex flex-col md:flex-row justify-end items-center md:justify-center md:items-end space-y-2 md:space-y-0 md:space-x-2'>
+      <Head>
+        <title>LDK UNISMA - Program</title>
+      </Head>
+      <div className="relative h-screen">
+        <div className="absolute h-screen w-full z-40 flex flex-col justify-center items-center">
+          <div className="w-full h-3/5 flex flex-col md:flex-row justify-end items-center md:justify-center md:items-end space-y-2 md:space-y-0 md:space-x-2">
             <input
               type="text"
               className="w-5/6 md:w-2/5 h-12 text-slate-400 pl-2 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-500"
               placeholder="Cari Program..."
-              onChange={event => setQuery(event.target.value)}
+              onChange={(event) => setQuery(event.target.value)}
             />
-            <button 
+            <button
               className="px-5 py-2 h-12 rounded-md bg-cyan-500 shadow-sm shadow-black text-white font-semibold"
               onSubmit={searching}
             >
-            Search
-            <FontAwesomeIcon 
-              icon={faSearch}
-              className="text-white ml-2"
-            ></FontAwesomeIcon>
-          </button>
+              Search
+              <FontAwesomeIcon
+                icon={faSearch}
+                className="text-white ml-2"
+              ></FontAwesomeIcon>
+            </button>
           </div>
-          <div className='w-full h-2/5 flex flex-col justify-end items-center'>
+          <div className="w-full h-2/5 flex flex-col justify-end items-center">
             <Title title="Program LDK" />
           </div>
         </div>
@@ -88,27 +89,31 @@ function ProgramPage() {
           modules={[Autoplay, Pagination]}
           className="mySwiper"
         >
-          {carouselArray.map((carousel, index)=> {
+          {carouselArray.map((carousel, index) => {
             return (
               <SwiperSlide key={index}>
                 <div className={carousel}></div>
               </SwiperSlide>
-            )
+            );
           })}
         </Swiper>
       </div>
-      <div className='container mx-auto my-5'>
-        <TabFilter resultQuery={resultQuery} resultData={resultData} setResultData={setResultData} />
+      <div className="container mx-auto my-5">
+        <TabFilter
+          resultQuery={resultQuery}
+          resultData={resultData}
+          setResultData={setResultData}
+        />
         {resultQuery.length ? (
           <CourseCard resultQuery={resultData} />
         ) : (
-          <div className='h-screen flex justify-center items-center'>
+          <div className="h-screen flex justify-center items-center">
             <SearchNotFound text="Pencarian tidak ditermukan.." />
           </div>
         )}
       </div>
     </>
-  )
+  );
 }
 
-export default ProgramPage
+export default ProgramPage;
