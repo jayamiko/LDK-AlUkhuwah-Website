8e0225fc8694eab6568e55b85a7c0aa8258ad64c
jayamiko <jayamiko4@gmail.com>
2022-09-18 09:21:41 +0700

    Fixed import Head

--- pages/program/index.jsx
+++ pages/program/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Head from "next/head";
 import { blog } from "../../data/data";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
