563711bbd3f3407ebf0a52a5c7cedefb8b9ca7de
jayamiko <jayamiko4@gmail.com>
2022-10-21 16:13:43 +0700

    Added Modal at Calendar

--- components/Calendar/Calendar.jsx
+++ components/Calendar/Calendar.jsx
@@ -1,12 +1,14 @@
+import React, { useState } from "react";
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React from "react";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { schedule } from "../../data/schedule/schedule";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -19,12 +21,21 @@ const localizer = dateFnsLocalizer({
   locales,
 });
 
-const eventComponent = ({ schedule }) =>
+const eventComponent = ({
+  schedule,
+  show,
+  handleShow,
+  handleClose,
+  title,
+  setTitle,
+}) =>
   function event(props) {
     return (
       <div className="rbc-row-segment">
         <div
           className="rbc-event h-[24px] sm:h-[26px]"
+          name={props.title}
+          title={props.title}
           style={{
             background:
               props.event.status == "learn"
@@ -35,11 +46,42 @@ const eventComponent = ({ schedule }) =>
                 ? "orange"
                 : "red",
           }}
+          onClick={(e) => handleShow(e)}
         >
           <div className="rbc-event-content" title={props.title}>
-            <h5 className="text-xs md:text-sm lg:text-base">{props.title}</h5>
+            <h5
+              className="text-xs md:text-sm lg:text-base"
+              onClick={(e) => setTitle(e.target.textContent)}
+            >
+              {props.title}
+            </h5>
           </div>
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title className="uppercase font-bold text-cyan-500">
+              {props.event.status === "learn"
+                ? "Sesi Materi"
+                : props.event.status === "presentation"
+                ? "Sesi Presentasi"
+                : props.event.status === "task"
+                ? "Tugas"
+                : "Materi Tambahan"}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="flex flex-col items-center">
+              <h5 className="font-medium">Materi :</h5>
+              <h3 className="font-medium text-cyan-500">{title}</h3>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="info" className="text-white" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   };
@@ -47,12 +89,22 @@ const eventComponent = ({ schedule }) =>
 eventComponent.displayName = "Event Component";
 
 function FullCalendarComp() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState(null);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = (e) => {
+    setShow(true);
+    setTitle(e.target.textContent);
+  };
   return (
     <div className="container mx-auto">
       <h1 className="font-mountains text-cyan-500 text-center lg:text-6xl">
         Schedule
       </h1>
-      <div className="flex justify-center space-x-5 my-4">
+      <div className="flex justify-center space-x-3 lg:space-x-5 my-4">
         <div className="flex items-center space-x-2">
           <div className="bg-cyan-500 w-3 h-3"></div>
           <label>Materi</label>
@@ -77,7 +129,14 @@ function FullCalendarComp() {
         endAccessor="end"
         style={{ height: 600 }}
         components={{
-          event: eventComponent({ schedule }),
+          event: eventComponent({
+            schedule,
+            show,
+            handleShow,
+            handleClose,
+            title,
+            setTitle,
+          }),
         }}
       />
     </div>
