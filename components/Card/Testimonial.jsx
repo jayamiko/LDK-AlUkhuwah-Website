a5fae2a100fcd98730b5a297782f7edda435dd4a
jayamiko <jayamiko4@gmail.com>
2022-10-28 13:09:49 +0700

    Added Testimonial Content

--- components/Card/Testimonial.jsx
+++ components/Card/Testimonial.jsx
@@ -5,6 +5,7 @@ import SectionSubtitle from "../utils/SectionSubtitle";
 import logo from "../../public/images/ldk-icon.png";
 import Slider from "react-slick";
 import classes from "../../styles/testimonial.module.css";
+import { testimonial } from "../../data/testimonial/testimonial";
 
 const Testimonial = () => {
   const settings = {
@@ -30,60 +31,26 @@ const Testimonial = () => {
 
         <Col lg="6" md="6">
           <SectionSubtitle subtitle="Testimonial" />
-          <h4 className="font-edu font-black my-2">Apa yang alumni katakan?</h4>
+          <h4 className="font-edu font-black mt-2 mb-10">
+            Apa yang mereka katakan?
+          </h4>
 
           <Slider {...settings}>
-            <div
-              className={`${classes.testimonial__item} border border-black h-[200px]`}
-            >
-              <h5 className="text-gray-400 italic">Belum ada ulasan</h5>
-              {/* <div className={`${classes.testimonial__client}`}>
-                <Image
-                  alt="client-img"
-                  src="/images/webinar/webinar.jpg"
-                  width="50px"
-                  height="50px"
-                  className=" rounded-2"
-                />
-                <div>
-                  <h6>Henry Nichols</h6>
-                  <h6>Web Developer</h6>
+            {testimonial.map((testi, index) => {
+              return (
+                <div key={index} className={`${classes.testimonial__item}`}>
+                  <div className={`${classes.testimonial__client}`}>
+                    <div className="font-bold">
+                      <h6>{testi.name}</h6>
+                      <h6 className="text-cyan-500">
+                        {testi.studi} - {testi.year}
+                      </h6>
+                    </div>
+                  </div>
+                  <i className="text-xs sm:text-sm">{testi.message}</i>
                 </div>
-              </div>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi saepe eveniet dolores molestias nisi ullam quis
-                delectus inventore, quidem beatae ipsa pariatur doloribus
-                corrupti! Quisquam expedita minima, neque et quis, fugiat hic
-                iste possimus vitae perspiciatis consequatur quod modi
-                dignissimos.
-              </p> */}
-            </div>
-            {/* <div className={`${classes.testimonial__item}`}>
-              <div className={`${classes.testimonial__client}`}>
-                <Image
-                  alt="client-img"
-                  src="/images/webinar/webinar.jpg"
-                  width="50px"
-                  height="50px"
-                  className=" rounded-2"
-                />
-
-                <div>
-                  <h6>Henry Nichols</h6>
-                  <h6>Web Developer</h6>
-                </div>
-              </div>
-
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi saepe eveniet dolores molestias nisi ullam quis
-                delectus inventore, quidem beatae ipsa pariatur doloribus
-                corrupti! Quisquam expedita minima, neque et quis, fugiat hic
-                iste possimus vitae perspiciatis consequatur quod modi
-                dignissimos.
-              </p>
-            </div> */}
+              );
+            })}
           </Slider>
         </Col>
       </Row>
---
+++ data/testimonial/testimonial.js
@@ -0,0 +1,16 @@
+export const testimonial = [
+  {
+    name: "Sahrul Noval",
+    studi: "Perbankan Syariah",
+    year: "2018",
+    message:
+      "LDK adalah sebuah rumah. Rumah yang memberikan ketenangan dengan nilai-nilai persatuan didalamnya. Rumah yang memberikan sebuah pelajaran baik itu dalam membina keislaman dan keorganisasian. Dan seperti yang disebut rumah, sejauh apapun kita pergi kita selalu ingin kembali.",
+  },
+  {
+    name: "Jaya Miko Yoga Pratama",
+    studi: "Pendidikan Agama Islam",
+    year: "2019",
+    message:
+      "Dulu pendapatku berorganisasi itu ga penting. Namun nyatanya banyak hal yang belum aku ketahui. Dengan berorganisasi, kita semakin berkembang dalam segala aspek apapun yang tidak akan pernah didapat jika hanya diam di bangku kuliah saja. Dan yang paling berkesan buatku di LDK adalah ukhuwah Islamiyahnya sangat kuat :)",
+  },
+];
