093b8678ab584c767a673784ec7a735a7b76d376
jayamiko <jayamiko4@gmail.com>
2022-10-28 16:24:06 +0700

    Added Content Testimoni

--- data/testimonial/testimonial.js
+++ data/testimonial/testimonial.js
@@ -13,4 +13,18 @@ export const testimonial = [
     message:
       "Dulu pendapatku berorganisasi itu ga penting. Namun nyatanya banyak hal yang belum aku ketahui. Dengan berorganisasi, kita semakin berkembang dalam segala aspek apapun yang tidak akan pernah didapat jika hanya diam di bangku kuliah saja. Dan yang paling berkesan buatku di LDK adalah ukhuwah Islamiyahnya sangat kuat :)",
   },
+  {
+    name: "Septiana",
+    studi: "Manajemen",
+    year: "2019",
+    message:
+      "Bagi saya LDK adalah keluarga. Kenapa saya bilang keluarga karena cuma di LDK kating-kating nya pada peduli mulai dari nanyain kabar, maupun sharing tentang hal apapun. Di LDK tidak hanya belajar organisasi tapi kita juga belajar tentang keislaman, dan saya sangat bersyukur teman-teman nya pada baik-baik banget, kalau saya salah selalu diingetin. Dan saya mengucapkan terima kasih kepada Kak Wahyu Nisa Listriyani yang udah membawa saya masuk LDK, mungkin kalau bukan kak Nisa yg ngenalin saya dengan LDK hati saya belum tergerak ikut bergabung dengan organisasi ini.",
+  },
+  {
+    name: "Bagus Pribadi",
+    studi: "Teknik Mesin",
+    year: "2020",
+    message:
+      "LDK adalah sebuah lingkaran orang orang yang mengajakmu untuk menjadi lebih baik dari hari kemarin, di dalamnya bukan hanya belajar memahami nilai nilai keislaman namun juga kehidupan sehari-hari kita.",
+  },
 ];
