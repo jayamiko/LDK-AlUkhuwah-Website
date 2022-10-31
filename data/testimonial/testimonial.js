b4873117d2c1d684137211f0d7f2ce73f31c538c
jayamiko <jayamiko4@gmail.com>
2022-10-31 15:52:00 +0700

    Added testimoni Content

--- data/testimonial/testimonial.js
+++ data/testimonial/testimonial.js
@@ -27,4 +27,19 @@ export const testimonial = [
     message:
       "LDK adalah sebuah lingkaran orang orang yang mengajakmu untuk menjadi lebih baik dari hari kemarin, di dalamnya bukan hanya belajar memahami nilai nilai keislaman namun juga kehidupan sehari-hari kita.",
   },
+  {
+    name: "Fatimah Nur A",
+    studi: "Sastra Inggris",
+    year: "2021",
+    message:
+      "LDK is my second home. Rumah kedua fatim yg isinya kk²  yg masyaAllah. Kk yang baik banget, sabar dan the best support system untuk urusan dunia maupun akhirat. LDK provides so many good things inside, dari punya temen buat olahraga, punya temen buat jalan², temen buat belajar bahkan temen buat kajian pokoknya dunia akhirat berjalan berdampingan. Semoga Allah mudahkan semua urusan LDK.",
+  },
+  {
+    name: "Lisna Mutia Jingga",
+    studi: "Psikologi",
+    year: "2019",
+    message:
+      `Bagi saya LDK adalah sebuah anugerah terindah.Kebutuhan yang selama ini saya cari ternyata ada di LDK.Meskipun pertumbuhan rasa nyaman itu lumayan lama,tapi so far setelah dijalani setiap prosesnya Alhamdulillah bisa beradaptasi dengan baik.
+Selain menjadi tempat untuk menimba ilmu agama,organisasi ini sudah menjadi tempat untuk saya bisa merasakan kehangatan dalam persaudaraan.`,
+  },
 ];
