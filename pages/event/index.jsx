1dc0c9078cbe966744a73c2565d1a2884cbbce48
jayamiko <jayamiko4@gmail.com>
2022-11-04 16:25:57 +0700

    Import link/next

--- pages/event/index.jsx
+++ pages/event/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 import Image from "next/image";
 // import HeadTop from "../../components/layout/Header/Head";
 import Header from "../../components/layout/Header/Header";
